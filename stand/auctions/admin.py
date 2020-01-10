d4b065e7809df04156f2c32f597a1b1443f9ab9f
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-01-09 19:44:19 -0800

    model fields working

--- stand/auctions/admin.py
+++ stand/auctions/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Auction, Item, Organization
+from .models import Auction, Item, Organization, Order, OrderItem
 # Register your models here.
 admin.site.register(Auction)
 admin.site.register(Item)
 admin.site.register(Organization)
+admin.site.register(Order)
+admin.site.register(OrderItem)
